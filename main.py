658268653ef3d3c475f987a2ee32537717e75079
Iamdeveloper <iamdeveloper@gmail.com>
2022-10-23 15:02:45 +0530

    functionality

--- main.py
+++ main.py
@@ -8,8 +8,21 @@ def number_gen():
 def start_game(score1,score2):
     user1 = number_gen()
     user2 = number_gen()
-
+    if (user1==0):
+        pyscript.write("img1","<img src='./assets/rock.jpg' alt='paper image'>")   
+    elif(user1==1):
+        pyscript.write("img1","<img src='./assets/paper.jpg' alt='paper image'>")  
+    elif(user1==2):
+        pyscript.write("img1","<img src='./assets/scissor.jpg' alt='paper image'>")  
+       
     # dict = {0:'rock', 1:'paper', 2:'scissor'}
+    if (user2==0):
+        pyscript.write("img2","<img src='./assets/rock.jpg' alt='paper image'>")   
+    elif(user2==1):
+        pyscript.write("img2","<img src='./assets/paper.jpg' alt='paper image'>")  
+    elif(user2==2):
+        pyscript.write("img2","<img src='./assets/scissor.jpg' alt='paper image'>")  
+      
     a,b = compare(user1,user2,score1, score2)
     #compare(score1, score2, dict)
     return a,b
@@ -49,8 +62,21 @@ def compare(user1,user2,score1, score2):
     
     return score1, score2
 
-score1=0
-score2=0
-    
+
 # score1,score2=start_game(score1,score2)
 # print(score1,score2)
+score1=0
+score2=0
+
+def show_output(*args):
+    a,b=start_game(score1,score2)
+    pyscript.write("score1",a)
+    pyscript.write("score2",b)
+
+def reset_func(*args):
+    pyscript.write("score1",0)
+    pyscript.write("score2",0)    
+
+
+
+
